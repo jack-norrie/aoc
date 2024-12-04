206a8ca6b08d3cf241f7fc8e7d005820255baacf
jack-norrie <jacknorrie6785@gmail.com>
2024-12-04 18:05:09 +0000

    Add existing project files to git.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Advent of Code 2023 - C++ Solutions
+
+This repository contains my solutions for [Advent of Code 2024](https://adventofcode.com/2024) implemented in C++. This project serves as my learning journey into C++ programming.
+
---
+++ src/day_2/day_2_1.cpp
@@ -0,0 +1,58 @@
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <string>
+
+int main() {
+  int res = 0;
+
+  std::ifstream input_file("data/day_2/input");
+
+  if (!input_file.is_open()) {
+    std::cerr << "Error: Could not open file" << std::endl;
+  }
+
+  std::string line;
+  while (std::getline(input_file, line)) {
+    bool safe = true;
+
+    std::istringstream line_stream(line);
+
+    int first_number;
+    int second_number;
+    if (line_stream >> first_number and line_stream >> second_number) {
+
+      // Determine initial direction and wether initial delta is valid.
+      int delta = second_number - first_number;
+      int sign;
+      if (delta >= 0) {
+        sign = 1;
+      } else {
+        sign = -1;
+      }
+      if (delta * sign <= 0 or delta * sign > 3) {
+        safe = false;
+      }
+
+      // Check remaining deltas have correct diretion and magnitude.
+      int prev_number = second_number;
+      int number;
+      while (line_stream >> number and safe) {
+        delta = number - prev_number;
+        if (delta * sign <= 0 or delta * sign > 3) {
+          safe = false;
+        }
+        prev_number = number;
+      }
+    }
+
+    // Increment safe count if line is safe.
+    if (safe) {
+      res += 1;
+    }
+  }
+  std::cout << res << std::endl;
+
+  return 0;
+}
