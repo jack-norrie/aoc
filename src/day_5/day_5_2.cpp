ab463775e1fedfb648345894948f52e07175cf73
jack-norrie <jacknorrie6785@gmail.com>
2024-12-07 17:38:32 +0000

    Add asymptotic analysis for d5p2.

--- src/day_5/day_5_2.cpp
+++ src/day_5/day_5_2.cpp
@@ -57,7 +57,6 @@ std::vector<size_t> fix_topological_sort(
   return correct_topological_sort;
 }
 void update_dependencies(
-
     const std::unordered_map<size_t, std::vector<size_t>> &dependency_graph,
     std::unordered_set<size_t> &seen_dependencies, size_t &dependee) {
   if (dependency_graph.contains(dependee)) {
@@ -121,12 +120,18 @@ std::vector<size_t> get_topological_sort(std::string line) {
 }
 
 int main() {
+  // T(V, E, q)  = O(qE)
+  // M(V, E, q)  = O(E)
+  // - V: number of verticies in the depdency graph, i.e. number of rules.
+  // - E: number of edgess in the depdency graph, i.e. number of pages.
+  // - q: number of update queries, i.e. number of updates.
   std::fstream input_file("data/day_5/input");
   if (!input_file.is_open()) {
     std::cout << "Erorr: Could not open file." << std::endl;
     return 1;
   }
 
+  // O(V+E) = O(E)
   std::unordered_map<size_t, std::vector<size_t>> dependency_graph =
       build_dependency_graph(input_file);
 
@@ -135,15 +140,15 @@ int main() {
   while (std::getline(input_file, line)) {
     std::vector<size_t> topological_sort = get_topological_sort(line);
 
+    // O(V + E) = O(E)
     if (!check_update_validity(dependency_graph, topological_sort)) {
-      // int division is floor division, which for zero indexing returns mid
-      // value.
       std::cout << "Incorrect Update: ";
       for (size_t num : topological_sort) {
         std::cout << num << " ";
       }
       std::cout << std::endl;
 
+      // O(V + E) = O(E)
       topological_sort =
           fix_topological_sort(dependency_graph, topological_sort);
 
