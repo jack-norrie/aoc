ccdcd9061f5f02c1126bc5a52a1826cbf35ff59b
jack-norrie <jacknorrie6785@gmail.com>
2024-12-07 23:32:51 +0000

    Add d6p2 brute force.

--- src/day_6/day_6_2.cpp
+++ src/day_6/day_6_2.cpp
@@ -1,6 +1,6 @@
+#include <cstddef>
 #include <fstream>
 #include <iostream>
-#include <sstream>
 #include <stdexcept>
 #include <string>
 #include <tuple>
@@ -12,6 +12,12 @@ namespace std {
 template <> struct hash<tuple<int, int>> {
   size_t operator()(const tuple<int, int> &t) const {
     return hash<int>()(get<0>(t)) ^ (hash<int>()(get<1>(t)) << 1);
+  }
+};
+template <> struct hash<tuple<int, int, int>> {
+  size_t operator()(const tuple<int, int, int> &t) const {
+    return hash<int>()(get<0>(t)) ^ (hash<int>()(get<1>(t)) << 1) ^
+           (hash<int>()(get<2>(t)) << 1);
   }
 };
 } // namespace std
@@ -48,7 +54,7 @@ get_starting_position(const std::vector<std::vector<char>> &grid) {
 }
 
 std::vector<std::vector<char>> get_grid() {
-  std::ifstream input_file("data/day_6/test_case_1");
+  std::ifstream input_file("data/day_6/input");
   if (!input_file.is_open()) {
     std::cout << "Error: Could not open file";
   }
@@ -68,53 +74,55 @@ std::vector<std::vector<char>> get_grid() {
   return grid;
 }
 
-bool check_obstruction_causes_cycle(
-    const std::vector<std::vector<char>> &grid,
-    const std::unordered_set<std::tuple<int, int>> &visited_positions, int r,
-    int c, const std::tuple<int, int> &direction) {
+std::unordered_map<char, std::tuple<int, int>> state_to_direction{
+    {'^', {-1, 0}}, {'>', {0, 1}}, {'v', {1, 0}}, {'<', {0, -1}}};
 
-  int d_r = std::get<0>(direction);
-  int d_c = std::get<1>(direction);
+std::unordered_map<char, char> state_transitions = {
+    {'^', '>'}, {'>', 'v'}, {'v', '<'}, {'<', '^'}};
+
+std::unordered_map<char, int> state_to_num = {
+    {'^', 0}, {'>', 1}, {'v', 2}, {'<', 3}};
+
+bool check_obstruction_causes_cycle(const std::vector<std::vector<char>> &grid,
+                                    int r, int c, char state) {
+
+  std::unordered_set<std::tuple<int, int, int>> seen_states;
+  seen_states.insert(std::tuple(r, c, state_to_num[state]));
+
+  // Change state to simulate an obstruction.
+  state = state_transitions[state];
 
   while (check_in_bounds(grid, r, c)) {
+    std::tuple<int, int> direction = state_to_direction[state];
+    int d_r = std::get<0>(direction);
+    int d_c = std::get<1>(direction);
+
     int n_r = r + d_r;
     int n_c = c + d_c;
 
-    if (!check_in_bounds(grid, n_r, n_c)) {
-      // Left the grid without a cycle.
-      return false;
-    } else if (grid[n_r][n_c] == '#') {
-      // Potential cycle if we have already visisted this path.
-      return visited_positions.contains(std::tuple(r, c));
-    } else {
+    // If not in bounds then update and allow while loop to end.
+    if (!check_in_bounds(grid, n_r, n_c) || grid[n_r][n_c] != '#') {
+      if (seen_states.contains(std::tuple(r, c, state_to_num[state]))) {
+        return true;
+      }
+
       r = n_r;
       c = n_c;
+    } else {
+      state = state_transitions[state];
     }
   }
-  throw std::runtime_error(
-      "Could not determine wether obstruction caused a cycle.");
+  return false;
 }
+size_t
+count_cycle_causing_obstructions(const std::vector<std::vector<char>> &grid,
+                                 int r, int c, char state) {
+  size_t res = 0;
 
-int main() {
-  int res = 0;
-
-  std::vector<std::vector<char>> grid = get_grid();
-
-  std::tuple<int, int> starting_pos = get_starting_position(grid);
-  int r = std::get<0>(starting_pos);
-  int c = std::get<1>(starting_pos);
-  char state = grid[r][c];
-
-  std::unordered_map<char, std::tuple<int, int>> state_to_direction{
-      {'^', {-1, 0}}, {'>', {0, 1}}, {'v', {1, 0}}, {'<', {0, -1}}};
-
-  std::unordered_map<char, char> state_transitions = {
-      {'^', '>'}, {'>', 'v'}, {'v', '<'}, {'<', '^'}};
-
-  std::unordered_set<std::tuple<int, int>> visited_positions;
+  std::unordered_set<std::tuple<int, int>> seen_states;
 
   while (check_in_bounds(grid, r, c)) {
-    visited_positions.insert(std::tuple(r, c));
+    seen_states.insert(std::tuple(r, c));
 
     std::tuple<int, int> direction = state_to_direction[state];
     int d_r = std::get<0>(direction);
@@ -123,24 +131,33 @@ int main() {
     int n_r = r + d_r;
     int n_c = c + d_c;
 
-    // See if placing an obstruction in front of you would cause a loop.
-    if (check_obstruction_causes_cycle(
-            grid, visited_positions, r, c,
-            state_to_direction[state_transitions[state]])) {
-      std::cout << r << '-' << c << std::endl;
-      res += 1;
-    }
-
     // If not in bounds then update and allow while loop to end.
     if (!check_in_bounds(grid, n_r, n_c) || grid[n_r][n_c] != '#') {
+      if (!(seen_states.contains(std::tuple(n_r, n_c))) &&
+          check_obstruction_causes_cycle(grid, r, c, state)) {
+        res += 1;
+      }
+
       r = n_r;
       c = n_c;
     } else {
       state = state_transitions[state];
     }
   }
+  return res;
+}
 
-  std::cout << "Number of cycle generating obstructions: " << res << std::endl;
+int main() {
+  std::vector<std::vector<char>> grid = get_grid();
+
+  std::tuple<int, int> starting_pos = get_starting_position(grid);
+  int r = std::get<0>(starting_pos);
+  int c = std::get<1>(starting_pos);
+  char state = grid[r][c];
+
+  size_t res = count_cycle_causing_obstructions(grid, r, c, state);
+
+  std::cout << res << std::endl;
 
   return 0;
 }
