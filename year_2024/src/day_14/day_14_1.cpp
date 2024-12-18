23ca8f4b02d8e6b26a4036519e6ccf62c8f38880
jack-norrie <jacknorrie6785@gmail.com>
2024-12-18 12:50:24 +0000

    Split d14p1 code up.

--- year_2024/src/day_14/day_14_1.cpp
+++ year_2024/src/day_14/day_14_1.cpp
@@ -7,6 +7,9 @@
 #include <tuple>
 #include <vector>
 
+const size_t N_COLS = 101;
+const size_t N_ROWS = 103;
+
 std::tuple<std::vector<std::array<int, 2>>, std::vector<std::array<int, 2>>>
 get_positions_and_velocicities() {
   std::ifstream input_file("data/day_14/input");
@@ -46,17 +49,10 @@ int remainder(int a, int b) {
   return remainder;
 }
 
-int main() {
-  std::tuple<std::vector<std::array<int, 2>>, std::vector<std::array<int, 2>>>
-      particle_tuple = get_positions_and_velocicities();
-  std::vector<std::array<int, 2>> positions = std::get<0>(particle_tuple);
-  std::vector<std::array<int, 2>> velocities = std::get<1>(particle_tuple);
-
-  std::array<size_t, 4> quadrants = {{0, 0, 0, 0}};
-
+void progress_time(std::vector<std::array<int, 2>> &positions,
+                   const std::vector<std::array<int, 2>> &velocities,
+                   const size_t t) {
   size_t n = positions.size();
-  size_t n_cols = 101;
-  size_t n_rows = 103;
   for (size_t i = 0; i < n; i++) {
     int pos_x = positions[i][0];
     int pos_y = positions[i][1];
@@ -64,24 +60,37 @@ int main() {
     int vel_x = velocities[i][0];
     int vel_y = velocities[i][1];
 
-    pos_x += vel_x * 100;
-    pos_y += vel_y * 100;
+    pos_x += vel_x * t;
+    pos_y += vel_y * t;
 
-    pos_x = remainder(pos_x, n_cols);
-    pos_y = remainder(pos_y, n_rows);
+    pos_x = remainder(pos_x, N_COLS);
+    pos_y = remainder(pos_y, N_ROWS);
+
+    positions[i][0] = pos_x;
+    positions[i][1] = pos_y;
+  }
+}
+
+size_t entropy(const std::vector<std::array<int, 2>> &positions) {
+  std::array<size_t, 4> quadrants = {{0, 0, 0, 0}};
+
+  size_t n = positions.size();
+  for (size_t i = 0; i < n; i++) {
+    int pos_x = positions[i][0];
+    int pos_y = positions[i][1];
 
     size_t quadrant = 0;
 
-    bool xq1 = pos_x < n_cols / 2;
-    bool xq2 = pos_x > n_cols / 2;
+    bool xq1 = pos_x < N_COLS / 2;
+    bool xq2 = pos_x > N_COLS / 2;
     if (!(xq1 ^ xq2)) {
       continue;
     } else if (xq2) {
       quadrant += 1;
     }
 
-    bool yq1 = pos_y < n_rows / 2;
-    bool yq2 = pos_y > n_rows / 2;
+    bool yq1 = pos_y < N_ROWS / 2;
+    bool yq2 = pos_y > N_ROWS / 2;
     if (!(yq1 ^ yq2)) {
       continue;
     } else if (yq2) {
@@ -91,12 +100,25 @@ int main() {
     quadrants[quadrant] += 1;
   }
 
-  size_t sf = 1;
-  for (auto q_count : quadrants) {
-    sf *= q_count;
+  size_t entropy = 1;
+  for (size_t q_count : quadrants) {
+    entropy *= q_count;
+    std::cout << q_count << std::endl;
   }
 
-  std::cout << sf << std::endl;
+  return entropy;
+}
+int main() {
+  std::tuple<std::vector<std::array<int, 2>>, std::vector<std::array<int, 2>>>
+      particle_tuple = get_positions_and_velocicities();
+  std::vector<std::array<int, 2>> positions = std::get<0>(particle_tuple);
+  std::vector<std::array<int, 2>> velocities = std::get<1>(particle_tuple);
+
+  progress_time(positions, velocities, 100);
+
+  size_t entropy_eval = entropy(positions);
+
+  std::cout << entropy_eval << std::endl;
 
   return 0;
 }
