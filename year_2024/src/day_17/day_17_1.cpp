f2cc0d27aa68009cea78b59ffc0ee0be034131a9
jack-norrie <jacknorrie6785@gmail.com>
2024-12-29 16:41:11 +0000

    Improve d17p1 in preperation for d17p2.

--- year_2024/src/day_17/day_17_1.cpp
+++ year_2024/src/day_17/day_17_1.cpp
@@ -52,12 +52,12 @@ size_t combo(const std::vector<size_t> &registers, const size_t &operand) {
   }
 }
 
-void compute(std::vector<size_t> &registers, size_t &i, const size_t &operator_,
-             const size_t &operand) {
+size_t compute(std::vector<size_t> &registers, size_t &i,
+               const size_t &operator_, const size_t &operand) {
   i += 2;
 
   if (operator_ == 0) {
-    registers[0] /= (1 << combo(registers, operand));
+    registers[0] = registers[0] >> combo(registers, operand);
   } else if (operator_ == 1) {
     registers[1] ^= operand;
   } else if (operator_ == 2) {
@@ -69,12 +69,42 @@ void compute(std::vector<size_t> &registers, size_t &i, const size_t &operator_,
   } else if (operator_ == 4) {
     registers[1] ^= registers[2];
   } else if (operator_ == 5) {
-    std::cout << combo(registers, operand) % 8 << ',';
+    return combo(registers, operand) % 8;
   } else if (operator_ == 6) {
-    registers[1] = registers[0] / (1 << combo(registers, operand));
+    registers[1] = registers[0] >> combo(registers, operand);
   } else if (operator_ == 7) {
-    registers[2] = registers[0] / (1 << combo(registers, operand));
+    registers[2] = registers[0] >> combo(registers, operand);
   }
+  return 8;
+}
+
+std::vector<size_t> run_program(const std::vector<size_t> &program,
+                                std::vector<size_t> &registers) {
+  std::vector<size_t> outputs = {};
+
+  size_t i = 0;
+  while (i < program.size()) {
+    size_t operator_ = program[i];
+    size_t operand = program[i + 1];
+
+    size_t output = compute(registers, i, operator_, operand);
+
+    if (output < 8) {
+      outputs.push_back(output);
+    }
+  }
+
+  return outputs;
+}
+
+void print_outputs(std::vector<size_t> outputs) {
+  for (size_t i = 0; i < outputs.size(); i++) {
+    std::cout << outputs[i];
+    if (i < outputs.size() - 1) {
+      std::cout << ',';
+    }
+  }
+  std::cout << std::endl;
 }
 
 int main() {
@@ -87,13 +117,9 @@ int main() {
 
   std::vector<size_t> program = get_program(input_file);
 
-  size_t i = 0;
-  while (i < program.size()) {
-    size_t operator_ = program[i];
-    size_t operand = program[i + 1];
-    compute(registers, i, operator_, operand);
-  }
-  std::cout << std::endl;
+  std::vector<size_t> outputs = run_program(program, registers);
+
+  print_outputs(outputs);
 
   return 0;
 }
